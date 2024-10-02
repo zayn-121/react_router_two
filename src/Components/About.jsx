7972ccc43c2617de51c2c174fa83e212379f5bf6
zayn-121 <zayedanwar121@gmail.com>
2024-10-02 21:08:58 +0530

    added about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='aboutdiv'>
+      <h1>About</h1>
+      <p>This is the About page.</p>
+    </div>
   )
 }
 
